a2a7d84c9b6e682197d8d0356f93e0bad86414c1
alisonsantosofc <alisonsantoso22@gmail.com>
2022-09-12 11:10:19 -0300

    create favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,7 +34,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;900&display=swap" rel="stylesheet" />
-          <link rel="shortcut icon" href="/images/icon.svg" />
+          <link rel="shortcut icon" href="/images/favicon.svg" />
         </Head>
 
         <body>
